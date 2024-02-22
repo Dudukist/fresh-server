b7e56adf6fb7d0f90e70f28edbfc2d012f49586d
Ali AliMohamadi <114885892+Dudukist@users.noreply.github.com>
2024-02-22 17:16:44 +0000

    Update fresh-config.sh

--- fresh-config.sh
+++ fresh-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-apt update -y && apt upgrade -y
+apt update -y
+apt upgrade -y
 apt install curl socat -y
 echo "127.0.0.1 `hostname`"  >> /etc/hosts
 echo "127.0.0.43 api.telegram.org"  >> /etc/hosts
