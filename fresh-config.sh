7dac4e57ed1e6c9296c00bef89a0fbd71d4231ed
Ali AliMohamadi <114885892+Dudukist@users.noreply.github.com>
2024-02-23 13:16:42 +0000

    Update fresh-config.sh

--- fresh-config.sh
+++ fresh-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-apt update -y
-apt upgrade -y
+apt update
+apt upgrade
 timedatectl set-timezone Asia/Tehran
-apt install curl socat -y
+apt install curl socat
 echo "127.0.0.1 `hostname`"  >> /etc/hosts
 echo "127.0.0.43 api.telegram.org"  >> /etc/hosts
 
