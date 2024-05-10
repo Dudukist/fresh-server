5832d733bd351adb98784b49f855599651350307
Ali AliMohamadi <114885892+Dudukist@users.noreply.github.com>
2024-05-10 21:50:20 +0000

    Update fresh-config.sh

--- fresh-config.sh
+++ fresh-config.sh
@@ -7,7 +7,11 @@ timedatectl set-timezone Asia/Tehran
 apt install curl socat
 echo "127.0.0.1 `hostname`"  >> /etc/hosts
 echo "127.0.0.43 api.telegram.org"  >> /etc/hosts
+-----------------------------
+# change ssh login permission
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+service ssh reload
+-----------------------------
 
 
 
